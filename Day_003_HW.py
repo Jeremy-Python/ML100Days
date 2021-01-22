4e1e392b76e378867054ec8677a2af324a954827
Jeremy-Python <77449720+Jeremy-Python@users.noreply.github.com>
2021-01-22 15:49:14 +0800

    Update Day_003_HW.py

--- Day_003_HW.py
+++ Day_003_HW.py
@@ -2,7 +2,7 @@ import numpy as np
 from decimal import Decimal
 # 第一題
 print(20*np.log10(20000/20))
-#
+# 第二題
 a=Decimal(np.power(10,(50/20))*20)
 b=Decimal(np.power(10,(30/20))*20)
 print(np.trunc(Decimal(a/b)))
