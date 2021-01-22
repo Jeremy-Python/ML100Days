4383710ae5cdb4cdbf9c6f62cb2b563e046bd2fe
Jeremy-Python <77449720+Jeremy-Python@users.noreply.github.com>
2021-01-22 15:48:06 +0800

    Add files via upload
    Day_003_HW
    2021/01/22

---
+++ Day_003_HW.py
@@ -0,0 +1,8 @@
+import numpy as np
+from decimal import Decimal
+# 第一題
+print(20*np.log10(20000/20))
+#
+a=Decimal(np.power(10,(50/20))*20)
+b=Decimal(np.power(10,(30/20))*20)
+print(np.trunc(Decimal(a/b)))
